d7254929a0cd2c400ddb21e9b8d4855c61a6dd45
rangerer <rainer.angerer@campaigning-bureau.com>
2017-08-01 12:16:53 +0200

    achnage chat icon again

--- src/components/Chatbot/Icons/ChatIcon.jsx
+++ src/components/Chatbot/Icons/ChatIcon.jsx
@@ -3,10 +3,11 @@ const ChatIcon = {
     render(h) {
         return (
 
-            <svg height="24" width="24"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 38 35">
-            <g fill="none" fill-rule="evenodd" transform="translate(.55 -1)">
-                <path stroke-width="2" d="M32.284 25.71c2.583-2.524 4.11-5.753 4.11-9.275 0-7.982-7.867-14.087-17.61-14.087-9.745 0-17.61 6.105-17.61 14.087 0 7.983 7.865 14.38 17.61 14.38 1.702 0 3.345-.233 4.93-.586l9.157 3.813-.584-8.335zM10.566 12.914H27m-16.434 7.043h9.39" stroke-linecap="square"/>
-            </g>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px"width="24px"  viewBox="0 0 16 16">
+                <g fill="none" stroke-miterlimit="10">
+                    <path d="M4.5 5.5h7m-7 3h7" />
+                    <path d="M15.5 7C15.5 3.41 12.142.5 8 .5S.5 3.41.5 7s3.358 6.5 7.5 6.5c.525 0 1.037-.048 1.532-.137L13.5 15.5v-4.09c1.238-1.16 2-2.706 2-4.41z" data-cap="butt"/>
+                </g>
             </svg>
 
         )
