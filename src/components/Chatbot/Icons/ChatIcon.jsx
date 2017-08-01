5c0be26450cb9390d5bc362a29cc9fa6f43dca30
rangerer <rainer.angerer@campaigning-bureau.com>
2017-08-01 12:23:09 +0200

    third icon change

--- src/components/Chatbot/Icons/ChatIcon.jsx
+++ src/components/Chatbot/Icons/ChatIcon.jsx
@@ -2,14 +2,12 @@ const ChatIcon = {
     'name': 'ChatIcon',
     render(h) {
         return (
-
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px"width="24px"  viewBox="0 0 16 16">
-                <g fill="none" stroke-miterlimit="10">
-                    <path d="M4.5 5.5h7m-7 3h7" />
-                    <path d="M15.5 7C15.5 3.41 12.142.5 8 .5S.5 3.41.5 7s3.358 6.5 7.5 6.5c.525 0 1.037-.048 1.532-.137L13.5 15.5v-4.09c1.238-1.16 2-2.706 2-4.41z" data-cap="butt"/>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24">
+                <g fill="none">
+                    <path d="M20.4 16.8C22 15.2 23 13.2 23 11c0-5-4.9-9-11-9S1 6 1 11s4.9 9 11 9c1.1 0 2.1-.1 3.1-.4L21 22l-.6-5.2z"/>
+                    <path d="M8 9h8m-8 4h5" />
                 </g>
             </svg>
-
         )
     }
 };
